be39e82ea1b2952cf2df64ecd369faf329062c43
sprajjwal <shashwatprajjwal@gmail.com>
2019-10-22 12:47:53 -0700

    [ADD]added anagram module

---
+++ Code/anagram.py
@@ -0,0 +1,9 @@
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    del argv[0] # deleting file name
+    assert len(argv) != 0, "No arguments were given" # no arguments give
+    query = argv[0]
+    anagrams = requests.get("http://www.anagramica.com/all/:" + query).json()
+    print([stuff for stuff in anagrams["all"] if len(stuff)>1 and stuff != query]) # list of ana
