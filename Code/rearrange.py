f091cb17890c8df04d267b6b943786dc1d04a70f
sprajjwal <shashwatprajjwal@gmail.com>
2019-10-23 15:59:01 -0700

    [FIX]removed extra whitespace at the end

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -4,8 +4,10 @@ from sys import argv
 if __name__ == "__main__":
     argc = len(argv)
     del argv[0] # deleting the file name
+    assert len(argv) != 0, "No arguments were given" # no arguments given
     rearr_str = ""
     while len(argv) > 0:
         rearr_str += argv.pop(argv.index(choice(argv)))
-        rearr_str += " "
+        if len(argv) != 1:
+            rearr_str += " "
     print(rearr_str)
