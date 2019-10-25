2e42939c4d6657110395b167f35e37343e7001a5
sprajjwal <sprajjwal@ucdavis.edu>
2019-10-24 21:00:49 -0700

    [ADD]histogram

---
+++ Code/histogram.py
@@ -0,0 +1,44 @@
+def histogram(words):
+    """ Makes a histogram from a list of words"""
+    hist = {}
+    for word in words:
+        word = word.lower()
+        if word in hist.keys():
+            hist[word] += 1
+        else:
+            hist[word] = 1
+    return hist
+
+def unique_word(hist):
+    """ counts number of unique words in a histogram"""
+    return len(hist.keys())
+
+def frequency(word, hist):
+    """ gives the frequency of word in the histogram"""
+    if word.lower() in hist.keys():
+        return hist[word.lower()]
+    else:
+        return 0
+
+def read_hist(f):
+    """Reads a histogram type file into a dictionary """
+    lines = f.readlines()
+    hist = {}
+    for line in lines:
+        line = line.split()
+        hist[line[0]] = line[1]
+    return hist
+
+def write_hist(file_name, hist):
+    with open(file_name, "w+") as f:
+        for key in hist.keys():
+            f.write(f"{key} {hist[key]}\n")
+
+if __name__ == "__main__":
+    with open("bro_code.txt", 'r') as f:
+        words = f.read().split()
+        hist = histogram(words)
+    print(hist["a"])
+    print(unique_word(hist))
+    print(frequency('a', hist))
+    write_hist("bro_hist.txt", hist)
