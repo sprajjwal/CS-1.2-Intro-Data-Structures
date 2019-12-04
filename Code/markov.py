577b73f36a4781b7348dd744202790c567518ce9
sprajjwal <sprajjwal@ucdavis.edu>
2019-12-03 23:33:51 -0800

    higher order markovs done

--- Code/markov.py
+++ Code/markov.py
@@ -3,23 +3,24 @@ import random
 import sys
 
 class Markogram(dict):
-    def __init__(self, words=None):
+    def __init__(self, words=None, order=1):
         """Initialize this histogram as a new dict and count given words."""
         super(Markogram, self).__init__()
         # Add properties to track useful word counts for this histogram
         self.types = 0  # Count of distinct word types in this histogram
         self.tokens = 1  # Total count of all word tokens in this histogram
+        self.order = order 
         # Count words in given list, if any
 
         if words is not None:
-            prev = words[0]
-            for index in range(1, len(words)-1):
-                self.add_count(words[index], prev)
-                prev = words[index]
-            for keys in self.keys():
-                self[keys] = Dictogram(self[keys])
+            prev = tuple(words[0:order])
+            for index in range(1, len(words) - order+1):
+                self.add_count(prev, tuple(words[index:index+order]))
+                prev = tuple(words[index:index+order])
+            for key in self.keys():
+                self[key] = Dictogram(self[key])
 
-    def add_count(self, word, prev):
+    def add_count(self, prev, word):
         """adds word to markogram"""
         if not prev in self.keys():
             self[prev] = []
@@ -34,24 +35,27 @@ class Markogram(dict):
     def get_string(self, len=1):
         """returns a string of len based on markov's chain"""
         start = random.choice(list(self.keys()))
-        strin = start
+        print(start)
+        strin = start[0]
         prev = start
         for _ in range(len-1):
             prev = self.sample(prev)
-            strin += f" {prev}"
+            strin += f" {prev[0]}"
         strin += "."
         return strin.capitalize()
 
 
-
 if __name__ == "__main__":
-    with open("bro_code.txt", 'r') as f:
-        words = f.read()
-    # words = "A man, a plan, a canal: Panama! A dog, a panic in a pagoda!"
-    dic = Markogram(words.split())
+    # with open("bro_code.txt", 'r') as f:
+    #     words = f.read()
+    words = "I went left, you went right, I went left, I went right,"
+    dic = Markogram(words.split(), order=3)
     # for key in dic.keys():
     #     print(f"{key}: {dic[key]}")
     # print(dic.sample('a'))
     # print(f"tokens: {dic.tokens}")
     # print(f"types: {dic.types}")
-    print(dic.get_string(10))
+    # print(dic.get_string(20))
+    for key in dic.keys():
+        print(f"{key}:  {dic[key]}")
+       
